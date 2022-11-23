b8aa8d45ceea5e8bb3290a813648f6737a1c9079
PatrickRayner <pa3q@outlook.com>
2022-11-23 20:01:42 -0300

    exericios operadores lógicos

--- fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
+++ fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
@@ -1,9 +1,11 @@
-let candidato = 45;
+const weekDay = "quarta-feira";
 
-if (candidato >= 80) {
-  console.log("Parabéns, você foi aprovada(o)!");
-} else if (candidato < 80 && candidato >= 60) {
-  console.log("Você está na nossa lista de espera");
-} else if (candidato < 60) {
-  console.log("Você foi reprovada(o)");
-}
+if (weekDay === "segunda-feira" || 
+    weekDay === "terça-feira" ||
+    weekDay === "quarta-feira" ||
+    weekDay === "quinta-feira" ||
+    weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+ } else {
+   console.log("FINALMENTE, descanso merecido UwU");
+ }
