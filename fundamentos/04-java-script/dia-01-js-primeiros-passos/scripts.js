b53b97d87799bb42dd37495c8be3b5ffce9fc7aa
PatrickRayner <pa3q@outlook.com>
2022-11-23 19:38:21 -0300

    exericios  Condições If e Else

--- fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
+++ fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
@@ -1,10 +1,9 @@
-const base = 5;
-const height = 8;
+let candidato = 45;
 
-const area = base * height;
-
-console.log (area);
-
-const perimeter = area + base + height;
-
-console.log (perimeter);
+if (candidato >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (candidato < 80 && candidato >= 60) {
+  console.log("Você está na nossa lista de espera");
+} else if (candidato < 60) {
+  console.log("Você foi reprovada(o)");
+}
