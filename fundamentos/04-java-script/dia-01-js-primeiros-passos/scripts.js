926bfaee78ffdd7a7a56a7816d56a6995a319ffe
PatrickRayner <pa3q@outlook.com>
2022-11-25 16:28:55 -0300

    exercicio positivo/negativo

--- fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
+++ fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
@@ -1,13 +1,9 @@
-let num1 = 8;
-let num2 = 15;
-let num3 = 23;
+let num = 4;
 
-if (num1 > num2 && num1 > num3) {
-  console.log('O maior número é:' + num1);
-
-} else if (num2 > num1 && num2 > num3) {
-  console.log('O maior número é:' + num2);
-  
+if (num > 0) {
+  console.log('positivo');
+} else if (num < 0) {
+  console.log('negativo');
 } else {
-  console.log('O maior número é:' + num3);
+  console.log('zero');
 };
