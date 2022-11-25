2d286667542880b9c3ce86e11ecc0f97011b8423
PatrickRayner <pa3q@outlook.com>
2022-11-25 16:27:57 -0300

    exercicio maior/menor com 3 números

--- fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
+++ fundamentos/04-java-script/dia-01-js-primeiros-passos/scripts.js
@@ -1,8 +1,13 @@
-let num1 = 12;
-let num2 = 30;
+let num1 = 8;
+let num2 = 15;
+let num3 = 23;
 
-if (num1 > num2) {
-  console.log(num1, "é maior que", num2);
+if (num1 > num2 && num1 > num3) {
+  console.log('O maior número é:' + num1);
+
+} else if (num2 > num1 && num2 > num3) {
+  console.log('O maior número é:' + num2);
+  
 } else {
-  console.log(num2, "é maior que", num1);
+  console.log('O maior número é:' + num3);
 };
