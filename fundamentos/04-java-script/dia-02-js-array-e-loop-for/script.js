fb95df3e5c5d3ab0e1c37203957218ff03de65ae
PatrickRayner <pa3q@outlook.com>
2022-11-26 08:13:15 -0300

    exercico dice

--- fundamentos/04-java-script/dia-02-js-array-e-loop-for/script.js
+++ fundamentos/04-java-script/dia-02-js-array-e-loop-for/script.js
@@ -1,5 +1,8 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-
-for (let value of names){
-  console.log(value);
+function dice () { 
+  return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+while (d1 === d2) {
+  d2 = dice();
 }
