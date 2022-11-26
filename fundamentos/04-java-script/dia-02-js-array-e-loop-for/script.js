ba738487a58cdd99283468086ff074b63002b037
PatrickRayner <pa3q@outlook.com>
2022-11-26 08:35:24 -0300

    // Calcule e imprima a média aritmética dos valores contidos no array;

--- fundamentos/04-java-script/dia-02-js-array-e-loop-for/script.js
+++ fundamentos/04-java-script/dia-02-js-array-e-loop-for/script.js
@@ -13,15 +13,20 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Some todos os valores contidos no array e imprima o resultado;
+// let sumOfNumbers = 0
+// for (let indexOfNumbers = 0; indexOfNumbers < numbers.length; indexOfNumbers += 1){
+//   sumOfNumbers += numbers[indexOfNumbers];
+// }
+// console.log(sumOfNumbers);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+//     A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 let sumOfNumbers = 0
 for (let indexOfNumbers = 0; indexOfNumbers < numbers.length; indexOfNumbers += 1){
   sumOfNumbers += numbers[indexOfNumbers];
 }
-console.log(sumOfNumbers);
-
-// Calcule e imprima a média aritmética dos valores contidos no array;
-
-//     A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+  average = sumOfNumbers/(numbers.length)
+console.log(average);
 
 // Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
