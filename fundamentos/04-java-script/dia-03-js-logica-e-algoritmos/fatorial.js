1fb1b188086f816988cd2ac984cb0726dec559e5
PatrickRayner <pa3q@outlook.com>
2022-11-29 16:09:12 -0300

    exercicio Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra

--- fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/fatorial.js
+++ fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/fatorial.js
@@ -7,3 +7,15 @@ num *= index;
 }
 console.log(num);
 }
+
+// // crie um algoritmo que imprima na tela o fatorial de 10.
+// num = 3;
+// let index = num-1;
+// if (num <= 0 || num === 1) {
+//   console.log('O número inserido não é um número fatorial');
+// } else { while (index >= 1) {
+// num *= index;
+// index--;
+// }
+// console.log(num);
+// }
