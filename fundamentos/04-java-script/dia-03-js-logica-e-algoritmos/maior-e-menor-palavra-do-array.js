d3cb3d537ff9da8285442f34bb82a95228d6cfc8
PatrickRayner <pa3q@outlook.com>
2022-11-29 16:17:39 -0300

    exercicio menor palavra do array

--- fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/maior-e-menor-palavra-do-array.js
+++ fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/maior-e-menor-palavra-do-array.js
@@ -1,11 +1,21 @@
 //  Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra. 
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+
 let bigWord = array[0];
+let smallWord = array[0];
 
 for (let index = 0; index < array.length; index += 1) {
   if (array[index].length > bigWord.length) {
     bigWord = array[index];
   }
 }
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallWord.length) {
+    smallWord = array[index];
+  }
+}
+
 console.log(bigWord);
+console.log(smallWord);
