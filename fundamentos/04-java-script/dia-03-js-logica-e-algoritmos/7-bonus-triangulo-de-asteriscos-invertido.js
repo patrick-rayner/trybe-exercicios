ace00d559e09ddcf930c90a562b8954fc1a5df62
PatrickRayner <pa3q@outlook.com>
2022-11-30 19:36:50 -0300

    7-bonus-triangulo-de-asteriscos-invertido

--- fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/7-bonus-triangulo-de-asteriscos-invertido.js
+++ fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/7-bonus-triangulo-de-asteriscos-invertido.js
@@ -6,3 +6,20 @@
 //   ***
 //  ****
 // *****
+let n = 5;
+let ast = "*";
+let space = '';
+let position = n - 1;
+
+for (let index = 0; index < n; index +=1) {
+  for (let index2 = 0; index2 < n; index2 +=1) {
+    if (index2 < position) {
+      space = space + ' ';
+    } else {
+      space = space + ast;
+    }
+ }
+ console.log(space);
+ space = '';
+ position -=1;    
+}
