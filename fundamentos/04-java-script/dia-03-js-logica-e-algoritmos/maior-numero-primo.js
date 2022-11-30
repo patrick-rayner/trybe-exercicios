ca3286df9c2d571cf3c92848bcf486be11444afc
PatrickRayner <pa3q@outlook.com>
2022-11-30 15:04:04 -0300

    retorna o maior numero primo entre 2 e 50

--- fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/maior-numero-primo.js
+++ fundamentos/04-java-script/dia-03-js-logica-e-algoritmos/maior-numero-primo.js
@@ -3,10 +3,14 @@
 let bigNumber = 0;
 
 for (let index = 2; index <= 50; index += 1) {
+  let isPrime = true;
   for (let index2 = 2; index2 < index; index2 +=1) {
-    if (!(index % index2 === 0)){
-      bigNumber = index2;
+    if (index % index2 === 0){
+      isPrime = false;
     }
   }
+  if (isPrime) {
+    bigNumber = index
+  }
 }
-console.log(index2);
+console.log(bigNumber);
