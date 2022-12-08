5a5b95c165a4d0dfff0455b63a1255e8f624cadf
PatrickRayner <pa3q@outlook.com>
2022-12-07 21:26:40 -0300

    feat: adiciona funções java script que alteram o texto do index.html dinamico

--- fundamentos/05-dom-eventos-e-web-storage/dia-01-js-dom-e-seletores/exercicio-manipulacao-dom/script.js
+++ fundamentos/05-dom-eventos-e-web-storage/dia-01-js-dom-e-seletores/exercicio-manipulacao-dom/script.js
@@ -3,8 +3,46 @@
   // document.getElementsByClassName();
   // document.getElementsByTagName();
 
+// Crie e execute uma função que mude o texto na tag <p>-----</p>, para uma descrição de como você se vê daqui a 2 anos; 
 const changeText = () => {
   let paragraph = document.getElementsByTagName('p')[1];
   paragraph.innerText = 'Ganhar 5.000 e estar em um empresa Ma-ra-vi-lho-sa!!';
 };
 changeText();
+
+// Crie e execute uma função que mude a cor do quadrado amarelo para rgb(76,164,109); 
+const changeToGreen = () => {
+let mainContent = document.querySelector('.main-content');
+mainContent.style.backgroundColor = 'rgb(76,164,109)';
+}
+changeToGreen ();
+
+// Crie e execute uma função que mude a cor do quadrado vermelho para branco; 
+const changeToWhite = () => {
+  let mainContent = document.querySelector('.main-content .center-content');
+  mainContent.style.backgroundColor = 'white';
+  }
+  changeToWhite ()
+
+// Crie e execute uma função que corrija o texto da tag <h1>; 
+const correctText = () => {
+  let paragraph = document.getElementsByTagName('h1')[0];
+  paragraph.innerText = 'Desafio - JavaScript';
+};
+correctText();
+
+// Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo;
+const upperText = () => {
+  let textToUpper = document.getElementsByTagName('p')[0];
+  textToUpper.style.textTransform = 'uppercase';
+};
+upperText ();
+
+// Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console. 
+const showContentP = () => {
+  let content = document.getElementsByTagName('p');
+  for (let index = 0; index < content.length; index += 1) {
+    console.log(content[index].innerHTML);
+  }
+};
+showContentP ();
